248700586a20b0c4c7151603564f9111cd09b8d1
Wilson Page <wilsonpage@me.com>
2013-03-18 00:11:56 +0000

    Instantiation tests

---
+++ test/tests/view.js
@@ -0,0 +1,35 @@
+
+buster.testCase('View', {
+  "Should add any children passed into the constructor": function() {
+    var children = [
+      {
+        module: 'pear'
+      },
+      {
+        module: 'orange'
+      }
+    ];
+
+    var view = new FruitMachine.View({
+      module: 'apple',
+      children: children
+    });
+
+    assert.equals(view.children().length, 2);
+  },
+
+  "Should create a model": function() {
+    var view = new FruitMachine.View({ module: 'apple' });
+    assert.isTrue(view.model instanceof FruitMachine.Model);
+  },
+
+  "Should setup an event listener to purge html cache when model changes": function() {
+    var spy = this.spy(FruitMachine.View.prototype, 'purgeHtmlCache');
+    var view = new FruitMachine.View({ module: 'orange' });
+
+    view.model.set('foo', 'bar');
+
+    assert.called(spy);
+    spy.restore();
+  }
+});
