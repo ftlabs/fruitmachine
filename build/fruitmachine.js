ecd2f789f05c1489313c95cef815492015eb1b85
Wilson Page <wilsonpage@me.com>
2013-05-19 20:10:18 +0100

    New build

--- build/fruitmachine.js
+++ build/fruitmachine.js
@@ -446,7 +446,7 @@ events(proto);
 
 var util = require('utils');
 var events = require('./events');
-var extend = require('../extend');
+var extend = require('extend');
 var mixin = util.mixin;
 
 /**
@@ -1319,77 +1319,7 @@ module.exports = function(fm) {
   return View;
 };
 
-},{"./events":8,"../extend":9,"utils":6}],9:[function(require,module,exports){
-/*jshint browser:true, node:true*/
-
-'use strict';
-
-/**
- * Module Dependencies
- */
-
-var mixin = require('utils').mixin;
-
-/**
- * Exports
- */
-
-module.exports = function(keys) {
-
-  return function(proto) {
-    var parent = this;
-    var child = function(){ return parent.apply(this, arguments); };
-
-    // Mixin static properties
-    // eg. View.extend.
-    mixin(child, parent);
-
-    // Make sure there are no
-    // keys conflicting with
-    // the prototype.
-    if (keys) protect(keys, proto);
-
-    // Set the prototype chain to
-    // inherit from `parent`, without
-    // calling `parent`'s constructor function.
-    function C() { this.constructor = child; }
-    C.prototype = parent.prototype;
-    child.prototype = new C();
-
-    // Add prototype properties
-    mixin(child.prototype, proto);
-
-    // Set a convenience property
-    // in case the parent's prototype
-    // is needed later.
-    child.__super__ = parent.prototype;
-
-    return child;
-  };
-};
-
-/**
- * Makes sure no properties
- * or methods can be overwritten
- * on the core View.prototype.
- *
- * If conflicting keys are found,
- * we create a new key prifixed with
- * a '_' and delete the original key.
- *
- * @param  {Array} keys
- * @param  {Object} ob
- * @return {[type]}
- */
-function protect(keys, ob) {
-  for (var key in ob) {
-    if (ob.hasOwnProperty(key) && ~keys.indexOf(key)) {
-      ob['_' + key] = ob[key];
-      delete ob[key];
-    }
-  }
-}
-},{"utils":6}],8:[function(require,module,exports){
+},{"./events":8,"utils":6,"extend":9}],8:[function(require,module,exports){
 
 /**
  * Module Dependencies
@@ -1476,6 +1406,76 @@ function propagate(view, args, event) {
 
 exports.fireStatic = events.prototype.fire;
 exports.off = events.prototype.off;
-},{"event":7}]},{},[1])(1)
+},{"event":7}],9:[function(require,module,exports){
+/*jshint browser:true, node:true*/
+
+'use strict';
+
+/**
+ * Module Dependencies
+ */
+
+var mixin = require('utils').mixin;
+
+/**
+ * Exports
+ */
+
+module.exports = function(keys) {
+
+  return function(proto) {
+    var parent = this;
+    var child = function(){ return parent.apply(this, arguments); };
+
+    // Mixin static properties
+    // eg. View.extend.
+    mixin(child, parent);
+
+    // Make sure there are no
+    // keys conflicting with
+    // the prototype.
+    if (keys) protect(keys, proto);
+
+    // Set the prototype chain to
+    // inherit from `parent`, without
+    // calling `parent`'s constructor function.
+    function C() { this.constructor = child; }
+    C.prototype = parent.prototype;
+    child.prototype = new C();
+
+    // Add prototype properties
+    mixin(child.prototype, proto);
+
+    // Set a convenience property
+    // in case the parent's prototype
+    // is needed later.
+    child.__super__ = parent.prototype;
+
+    return child;
+  };
+};
+
+/**
+ * Makes sure no properties
+ * or methods can be overwritten
+ * on the core View.prototype.
+ *
+ * If conflicting keys are found,
+ * we create a new key prifixed with
+ * a '_' and delete the original key.
+ *
+ * @param  {Array} keys
+ * @param  {Object} ob
+ * @return {[type]}
+ */
+function protect(keys, ob) {
+  for (var key in ob) {
+    if (ob.hasOwnProperty(key) && ~keys.indexOf(key)) {
+      ob['_' + key] = ob[key];
+      delete ob[key];
+    }
+  }
+}
+},{"utils":6}]},{},[1])(1)
 });
 ;
